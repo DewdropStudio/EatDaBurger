998627d0fe6758c5286a39aad0b3d423df8cc772
Rocio Avila <rocio.avila@northwestern.com>
2020-08-09 11:41:45 -0500

    Modified schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,9 +4,9 @@ DROP DATABASE IF EXISTS burgers_db;
     
     USE burgers_db;
     
-    CREATE TABLE burgers (
-    id INT AUTO_INCREMENT NOT NULL,
-    burger_name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN,
-    PRIMARY KEY (id)
+    CREATE TABLE `burgers` (
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `burger_name` VARCHAR(50) NOT NULL,
+    `devoured` BOOLEAN NOT NULL,
+    PRIMARY KEY ( `id` )
     );

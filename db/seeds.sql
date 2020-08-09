4317df07647d7cbdb77f802a1640636baf27c62a
Rocio Avila <rocio.avila@northwestern.com>
2020-08-09 12:05:25 -0500

    Added seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO 
+	burgers (id, burger_name, devoured)
+VALUES 
+	(1, "Bacon Burger", false),
+    (2, "Cheese Burger", false),
+    (3, "Vegan Burger", true);
+    
+-- displays all burgers  
+SELECT * FROM burgers;
